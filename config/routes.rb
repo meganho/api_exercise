fa4cbf0a64f0273ad94a3dfc01544d1fe56f586d
meganho <bbeeaanns@gmail.com>
2017-08-23 16:03:42 +0200

    auth_token post

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
 
    root "welcome#index"
-   
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api, :defaults => { :format => :json } do
      namespace :v1 do
+
+       post "/signup" => "auth#signup"
+       post "/login" => "auth#login"
+       post "/logout" => "auth#logout"
+
+       get "/reservations" => "reservations#index", :as => :reservations
        get "/trains"  => "trains#index", :as => :trains
        get "/trains/:train_number" => "trains#show", :as => :train
 
